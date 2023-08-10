f421ec3a895d106af282d95368454c52c05e7db7
ilias <essatenilas@gmail.com>
2023-08-10 23:53:53 +0300

    slider-mob

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,34 @@
-const swiper = new Swiper('.swiper', {
+/* const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
-    loop: true,
+    loop: true, */
   
     // If we need pagination
-    pagination: {
+/*     pagination: {
       el: '.swiper-pagination',
-    },
+    }, */
   
     // Navigation arrows
-    navigation: {
+/*     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    },
+    }, */
   
     // And if we need scrollbar
 /*     scrollbar: {
       el: '.swiper-scrollbar',
     }, */
-  });
+/*   }); */
+
+// Налаштування
+const myArray = [];
+
+// Змініть код лише під цим рядком
+let i = 0 
+
+while (i < 5 ){
+  myArray.unshift (i)
+  i++
+}
+
+console.log(myArray)
