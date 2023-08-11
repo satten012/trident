2e366e2fceb037d0e989ce0512cb4e3c3a413be2
ilias <essatenilas@gmail.com>
2023-08-11 23:54:41 +0300

    swiper-phone-2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,11 @@
-/* const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true, */
-  
-    // If we need pagination
-/*     pagination: {
-      el: '.swiper-pagination',
-    }, */
-  
-    // Navigation arrows
-/*     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }, */
-  
-    // And if we need scrollbar
-/*     scrollbar: {
-      el: '.swiper-scrollbar',
-    }, */
-/*   }); */
-
-// Налаштування
-const myArray = [];
-
-// Змініть код лише під цим рядком
-let i = 0 
-
-while (i < 5 ){
-  myArray.unshift (i)
-  i++
-}
-
-console.log(myArray)
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
