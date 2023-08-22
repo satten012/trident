a7c8fdb956eae994a8277d55bcd39a598417cbbc
ilias <essatenilas@gmail.com>
2023-08-22 23:47:08 +0300

    slider + headline + order blocks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
-var swiper = new Swiper(".mySwiper", {
+let swiper = new Swiper(".mySwiper", {
   loop:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+let swiper2 = new Swiper(".mySwiper-2", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
